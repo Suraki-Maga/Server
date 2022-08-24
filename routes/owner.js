eab3f05726a2de8ef288cb55601902a00d289c13
Dilshi Navodya <62331157+DilshiNavodya@users.noreply.github.com>
2022-08-25 00:17:59 +0530

    owner-update driver details added

--- routes/owner.js
+++ routes/owner.js
@@ -19,5 +19,47 @@ router.get("/getownerdetails", async (req, res, next) => {
       next(err)
     }
   })
+  
+  router.post("/registerDriver", async(req, res, next)=>{
+    try{
+      console.log(req.body)
+      const respond = Owner.registerDriver(req.body)
+      return res.status(200).json({respond})
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.post("/EditOwnerProfile",async(req, res, next)=>{
+    try{
+      console.log(req.body)
+      const respond = Owner.EditOwnerProfile(req.body)
+      return res.status(200).json({respond})
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.get("/getdriverdetails",async(req, res, next)=>{
+    try{
+      const driver = Owner.getdriverdetails(req.body)
+      driver.then(function(result) {
+        console.log(result)
+        return res.status(200).json({ result })
+      })
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.post("/EditOwnerDriverProfile",async(req,res,next)=>{
+    try{
+      console.log(req.body)
+      const respond = Owner.EditDriverProfile(req.body)
+      return res.status(200).json({respond})
+    } catch(err){
+      next(err)
+    }
+  })
 
 module.exports = router
