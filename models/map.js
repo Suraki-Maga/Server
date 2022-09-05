9f42dafcde6272d91b8c3ad61ee0c2c51d45b859
Zen_99 <2019cs161@stu.ucsc.lk>
2022-09-05 16:08:46 +0530

    load vehicle information

--- models/map.js
+++ models/map.js
@@ -1,30 +1,35 @@
-const db = require("../db")
+const db = require("../db");
 const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 const { query } = require("express");
 
-class Map{
-    static async loadStudentLocations(userName){
-        //select the van id from the userName
+class Map {
+  static async loadStudentLocations(username) {
+    //select the van id from the userName
+    //let query = `select schoolvan.id from schoolvan inner join driver_auth on driver_auth.id=schoolvan.driverid where driver_auth.username=$1`;
+    let query = `select id from driver_auth where username=$1`;
+    let result = await db.query(query, [username]);
+    console.log(result.rows);
+    query = "select id from schoolvan where driverid=$1";
+    result = await db.query(query, [result.rows[0].id]);
+    //DON'T LOAD ABSENT STUDENTS
+    let vanId = result.rows[0].id;
+    query = `select * from student_location inner join student on student.id=student_location.id where student.vanid=$1`;
+    result = await db.query(query, [vanId]);
+    console.log(result.rows);
+    return result.rows;
+  }
 
-        //DON'T LOAD ABSENT STUDENTS
-        const vanId=1
-        const query=`select * from student_location where van_id=$1`
-        const result=await db.query(query,[vanId])
-        console.log(result.rows)
-        return result.rows;
-    }
-
-    static async loadStudentDetails(credentials){
-        const requiredFields = ["studentId"]
-        requiredFields.forEach((property) => {
-          if (!credentials.hasOwnProperty(property)) {
-            throw new BadRequestError(`Missing ${property} in request body.`)
-          }
-        })
-        const query=`select * from student where id=$1`
-        const result=await db.query(query,[credentials.studentId])
-        return result.rows[0];
-    }
+  static async loadStudentDetails(credentials) {
+    const requiredFields = ["studentId"];
+    requiredFields.forEach((property) => {
+      if (!credentials.hasOwnProperty(property)) {
+        throw new BadRequestError(`Missing ${property} in request body.`);
+      }
+    });
+    const query = `select * from student where id=$1`;
+    const result = await db.query(query, [credentials.studentId]);
+    return result.rows[0];
+  }
 }
 
-module.exports=Map;
+module.exports = Map;
