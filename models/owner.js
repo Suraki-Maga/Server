5a0a7af810f65cb6a1d295c270edc5ddfe557cc0
Dilshi Navodya <62331157+DilshiNavodya@users.noreply.github.com>
2022-09-08 22:23:29 +0530

    owner model and routes changed

--- models/owner.js
+++ models/owner.js
@@ -20,16 +20,16 @@ class Owner {
     }
 
     static async registerDriver(credentials){
-        const requiredFields = ["fullname","licenceno","contact","nic","image"];
+        const requiredFields = ["name","licenseno","mobile","nic","url"];
         requiredFields.forEach((property) => {
           if (!credentials.hasOwnProperty(property)) {
             throw new BadRequestError(`Missing ${property} in request body.`);
           }
         });
-        const query = `INSERT INTO driver(fullname,licenceno,contact,nic,image)
-                        VALUES ($1,$2,$3,$4,$5) RETURNING id`
+        const query = `INSERT INTO driver(fullname,licenceno,contact,nic,image,avail)
+                        VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`
 
-        const result = await db.query(query,[credentials.name,credentials.licenseno,credentials.mobile,credentials.nic,credentials.url])
+        const result = await db.query(query,[credentials.name,credentials.licenseno,credentials.mobile,credentials.nic,credentials.url,1])
         return result.rows[0]
     }
 
@@ -50,7 +50,7 @@ class Owner {
 
     static async getdriverdetails(){
         const tbl = "driver"
-        const query = `select id,fullname,licenceno,contact,nic,image from driver`
+        const query = `select id,fullname,licenceno,contact,nic,image,avail from driver`
         const result=await db.query(query)
         return result.rows
     }
@@ -81,7 +81,7 @@ class Owner {
     }
     
     static async loadDriverDetails(credentials){
-        const requiredFields = ["id","fullname","licenceno","contact","nic","image"];
+        const requiredFields = ["data"];
         requiredFields.forEach((property) => {
           if (!credentials.hasOwnProperty(property)) {
             throw new BadRequestError(`Missing ${property} in request body.`);
@@ -94,20 +94,66 @@ class Owner {
     }
 
     static async registersclvan(credentials,ownerid){
-        const requiredFields = ["vehicleno","vehicletype","seats","charge","startlocation","driverid","frontimage","backimage","licensefront","licenseback"];
+        const requiredFields = ["vehicleno","vehicletype","seats","charge","startlocation","driverid","frontimg","backimg","licensefront","licenseback"];
         requiredFields.forEach((property) => {
           if (!credentials.hasOwnProperty(property)) {
             throw new BadRequestError(`Missing ${property} in request body.`);
           }
         });
+        
         console.log(ownerid)
         const query = `INSERT INTO schoolvan(vehicleno,vehicletype,seats,charge,startlocation,frontimage,backimage,licensefront,licenseback,driverid,ownerid)
                         VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING id`
 
         const result = await db.query(query,[credentials.vehicleno,credentials.vehicletype,credentials.seats,credentials.charge,credentials.startlocation,credentials.frontimg,credentials.backimg,credentials.licensefront,credentials.licenseback,credentials.driverid,ownerid])
+        const query1 = `UPDATE driver SET avail=$1 WHERE id=$2`
+        const result1 = await db.query(query1,[0,credentials.driverid])
         const schoolvanid = result.rows[0]
         console.log(schoolvanid)
         return schoolvanid
+    }
+
+    static async isuniquevehicleno(credentials){
+      const requiredFields = ["vehicleno"];
+        requiredFields.forEach((property) => {
+          if (!credentials.hasOwnProperty(property)) {
+            throw new BadRequestError(`Missing ${property} in request body.`);
+          }
+        });
+        const query = `select id from schoolvan where vehicleno=$1`
+        const vehicleno = await db.query(query,[credentials.vehicleno])
+        console.log(vehicleno.rows[0])
+        if(vehicleno.rows[0]===undefined){
+          return true;
+        } else {
+          return false
+        }
+    }
+
+    static async getschoolvandetails(ownerid){
+      const query = `select schoolvan.id,schoolvan.vehicleno,schoolvan.vehicletype,schoolvan.seats,schoolvan.charge,schoolvan.startlocation,schoolvan.ownerid,schoolvan.frontimage,driver.fullname from schoolvan INNER JOIN driver ON schoolvan.driverid=driver.id WHERE (schoolvan.approved=$1 AND schoolvan.ownerid=$2)`
+      const result = await db.query(query,[1,ownerid])
+      console.log(result)
+      return result.rows
+    }
+
+    static async addSchoolstoSchoolvan(credentials){
+      const requiredFields = ["school","schoolvanid"];
+      requiredFields.forEach((property) => {
+        if (!credentials.hasOwnProperty(property)) {
+          throw new BadRequestError(`Missing ${property} in request body.`);
+        }
+      });
+      const query = `INSERT INTO schoolvanschools(sclid,sclvanid) VALUES($1,$2)`
+      const result = await db.query(query,[credentials.school,credentials.schoolvanid])
+      return result.rows
+    }
+
+    static async getschoolsSchoolvan(){
+      const query = `select * from schoolvanschools`
+      const result = await db.query(query)
+      console.log(result)
+      return result.rows
     }
 }
 
