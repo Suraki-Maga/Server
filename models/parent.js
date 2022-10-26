85a4cb600c290fe5b9441cd004d226f6175cc3e7
Zen_99 <2019cs161@stu.ucsc.lk>
2022-10-26 16:57:51 +0530

    a day before presentation

--- models/parent.js
+++ models/parent.js
@@ -1,23 +1,20 @@
-const express = require("express")
-const db = require("../db")
-const security = require("../middleware/security")
+const express = require("express");
+const db = require("../db");
+const security = require("../middleware/security");
 
 class Parent {
+  //Function to retrieve id
+  static async fetchUserid(username) {
+    const query = `SELECT id FROM users WHERE username = $1`;
 
-    //Function to retrieve id
-    static async fetchUserid(username) {
-       
-        const query = `SELECT id FROM users WHERE username = $1`
-    
-        const userid = await db.query(query, [username])
-    
-        return userid.rows[0]
-      }
+    const userid = await db.query(query, [username]);
 
-    //Function to get children names
-    static async getChildren(userName) {
-    
-        const query = `Select student.id,student.fullname,school.name as school,
+    return userid.rows[0];
+  }
+
+  //Function to get children names
+  static async getChildren(userName) {
+    const query = `Select student.id,student.fullname,school.name as school,
         AGE(CURRENT_DATE, student.birthday),student.image,student.vanid,schoolvan.frontimage,schoolvan.backimage,owner.name 
         as ownername,owner.contact as ownercontact,driver.fullname 
         as drivername,driver.contact as drivercontact,student.payment_status,
@@ -26,21 +23,19 @@ class Parent {
         school ON student.school=school.id  left join schoolvan on student.vanid=
         schoolvan.id left join driver on schoolvan.driverid=driver.id 
         left join owner
-        ON schoolvan.ownerid=owner.id where users.username=$1`
-        const children=await db.query(query,[userName])
-        
-        if(children.rows){
-        return children.rows
-        }
-        else {
-            return "false";
-        }
-    }
+        ON schoolvan.ownerid=owner.id where users.username=$1`;
+    const children = await db.query(query, [userName]);
 
-      //Function to get children vehicle details
-      static async getChildVehicle(request) {
-    
-        const query = `Select student.id,student.fullname,school.name as school,
+    if (children.rows) {
+      return children.rows;
+    } else {
+      return "false";
+    }
+  }
+
+  //Function to get children vehicle details
+  static async getChildVehicle(request) {
+    const query = `Select student.id,student.fullname,school.name as school,
         AGE(CURRENT_DATE, student.birthday),student.image,student.vanid,schoolvan.frontimage,schoolvan.backimage,owner.name 
         as ownername,owner.contact as ownercontact,driver.fullname 
         as drivername,driver.contact as drivercontact,student.payment_status,
@@ -49,113 +44,119 @@ class Parent {
         school ON student.school=school.id  left join schoolvan on student.vanid=
         schoolvan.id left join driver on schoolvan.driverid=driver.id 
         left join owner
-        ON schoolvan.ownerid=owner.id where users.username=$1`
-        const childvehicle=await db.query(query,[request.studentid])
-        
-        if(childvehicle.rows){
-        return childvehicle.rows[0]
-        }
-        else {
-            return "false";
-        }
+        ON schoolvan.ownerid=owner.id where users.username=$1`;
+    const childvehicle = await db.query(query, [request.studentid]);
+
+    if (childvehicle.rows) {
+      return childvehicle.rows[0];
+    } else {
+      return "false";
     }
+  }
 
-    //Function to add a child
-    static async addChild(username,credentials) {
+  //Function to add a child
+  static async addChild(username, credentials) {
+    const userid = await Parent.fetchUserid(username);
 
-        const userid = await Parent.fetchUserid(username);
-    
-        const query = `INSERT INTO student(fullname,parentid,school,image,birthday)
-        VALUES ($1,$2,$3,$4,$5) RETURNING id`
+    const query = `INSERT INTO student(fullname,parentid,school,image,birthday)
+        VALUES ($1,$2,$3,$4,$5) RETURNING id`;
 
-        const result = await db.query(query,[credentials.name,userid.id,credentials.school,credentials.url,credentials.dob])
+    const result = await db.query(query, [
+      credentials.name,
+      userid.id,
+      credentials.school,
+      credentials.url,
+      credentials.dob,
+    ]);
 
-        const query1 = `INSERT INTO student_location(id,latitude,longitude)
-        VALUES ($1,$2,$3) RETURNING id`
-        
-        const result1 = await db.query(query1,[result.rows[0].id,credentials.latitude,credentials.longitude])
+    const query1 = `INSERT INTO student_location(id,latitude,longitude)
+        VALUES ($1,$2,$3) RETURNING id`;
 
-        if(result1)
-        {
-            return true
-        }
-        else {
-            return false
-        }
+    const result1 = await db.query(query1, [
+      result.rows[0].id,
+      credentials.latitude,
+      credentials.longitude,
+    ]);
+
+    if (result1) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-      //Function to get children names with the monthly charges
-      static async getChildrenRequest(userName,credentials) {
+  //Function to get children names with the monthly charges
+  static async getChildrenRequest(userName, credentials) {
+    const userid = await Parent.fetchUserid(userName);
+    console.log(userid);
+    console.log(credentials.vanid);
 
-        const userid = await Parent.fetchUserid(userName);
-        console.log(userid)
-        console.log(credentials.vanid)
-
-        const query = `SELECT student.id,student.fullname,student.vanid,student.school,student.parentid,
+    const query = `SELECT student.id,student.fullname,student.vanid,student.school,student.parentid,
         student_location.latitude,student_location.longitude,school.latitude,school.longtitude,
         schoolvan.charge from student INNER JOIN schoolvanschools ON student.school = schoolvanschools.sclid
         INNER JOIN student_location ON student_location.id =student.id INNER JOIN school ON school.id=student.school INNER JOIN schoolvan
-        ON schoolvan.id =schoolvanschools.sclvanid WHERE student.parentid=$1 and schoolvanschools.sclvanid=$2`
-        
-        const children=await db.query(query,[userid.id,credentials.vanid])
-        
-        if(children.rows){
-        return children.rows
-        }
-        else {
-            return "false";
-        }
+        ON schoolvan.id =schoolvanschools.sclvanid WHERE student.parentid=$1 and schoolvanschools.sclvanid=$2`;
+
+    const children = await db.query(query, [userid.id, credentials.vanid]);
+
+    if (children.rows) {
+      return children.rows;
+    } else {
+      return "false";
     }
+  }
 
+  //Function to send a request
+  static async sendRequest(credentials) {
+    const query = `INSERT INTO request(student_id,van_id,monthlycharge)
+        VALUES ($1,$2,$3) RETURNING id`;
 
-     //Function to send a request
-     static async sendRequest(credentials) {
+    const result = await db.query(query, [
+      credentials.studentid,
+      credentials.vanid,
+      credentials.monthlycharge,
+    ]);
 
-        const query = `INSERT INTO request(student_id,van_id,monthlycharge)
-        VALUES ($1,$2,$3) RETURNING id`
-
-        const result = await db.query(query,[credentials.studentid,credentials.vanid,credentials.monthlycharge])
-
-        if(result)
-        {
-            return true
-        }
-        else {
-            return false
-        }
+    if (result) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    //Function to leave from a van
-    static async leaveVan(credentials) {
+  //Function to leave from a van
+  static async leaveVan(credentials) {
+    const query = `UPDATE student SET vanid = NULL,  payment_status= NULL WHERE id=$1`;
 
-        const query = `UPDATE student SET vanid = NULL,  payment_status= NULL WHERE id=$1`
+    const result = await db.query(query, [credentials.studentid]);
 
-        const result = await db.query(query,[credentials.studentid])
-
-        if(result.rows[0]!=null)
-        {
-            return true
-        }
-        else {
-            return false
-        }
+    if (result.rows[0] != null) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    //Function to markabsent
-    static async markAbsent(credentials) {
+  //Function to markabsent
+  static async markAbsent(credentials) {
+    const query = `UPDATE student SET morning_state = $2,  evening_state= $3 WHERE id=$1 RETURNING id`;
 
-        const query = `UPDATE student SET morning_state = $2,  evening_state= $3 WHERE id=$1 RETURNING id`
+    const result = await db.query(query, [
+      credentials.studentid,
+      credentials.morning,
+      credentials.evening,
+    ]);
 
-        const result = await db.query(query,[credentials.studentid,credentials.morning,credentials.evening])
-
-        if(result.rows[0]!=null)
-        {
-            return true
-        }
-        else {
-            return false
-        }
+    if (result.rows[0] != null) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  static async getBio(username) {
+    const query = "";
+  }
 }
 
-module.exports = Parent
+module.exports = Parent;
--- routes/parent.js
+++ routes/parent.js
@@ -1,108 +1,135 @@
-const express = require("express")
-const router = express.Router()
-const Parent = require("../models/parent")
-const security = require("../middleware/security")
+const express = require("express");
+const router = express.Router();
+const Parent = require("../models/parent");
+const security = require("../middleware/security");
 
 //Route to get the children and the details
-  router.get("/children", security.requireAuthorizedUser, async (req, res) => {
-    try {
-      const username=res.locals.user.data
-      console.log(username)
-      const children=Parent.getChildren(username);
-      children.then(function(result) {
-        // console.log(result)
-        return res.status(200).json( {result} )
-      })
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server Error");
-    }
-  });
+router.get("/children", security.requireAuthorizedUser, async (req, res) => {
+  try {
+    const username = res.locals.user.data;
+    console.log(username);
+    const children = Parent.getChildren(username);
+    children.then(function (result) {
+      // console.log(result)
+      return res.status(200).json({ result });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-  //Route to add a child
-  router.post("/addChild",security.requireAuthorizedUser, async(req, res, next)=>{
-    try{
-      console.log("addChild")
+//Route to add a child
+router.post(
+  "/addChild",
+  security.requireAuthorizedUser,
+  async (req, res, next) => {
+    try {
+      console.log("addChild");
       const username = res.locals.user.data;
-      const respond = Parent.addChild(username,req.body)
-      respond.then(function(result){
-        console.log("add child")
-        return res.status(200).json({result})
-      })
-    } catch(err){
-      next(err)
+      const respond = Parent.addChild(username, req.body);
+      respond.then(function (result) {
+        console.log("add child");
+        return res.status(200).json({ result });
+      });
+    } catch (err) {
+      next(err);
     }
-  });
+  }
+);
 
-  //Route to get details of the van of a particular child
-  router.post("/childvandetails", security.requireAuthorizedUser, async (req, res) => {
+//Route to get details of the van of a particular child
+router.post(
+  "/childvandetails",
+  security.requireAuthorizedUser,
+  async (req, res) => {
     try {
-      const childvan=Parent.getChildVehicle(req.body);
-      childvan.then(function(result) {
+      const childvan = Parent.getChildVehicle(req.body);
+      childvan.then(function (result) {
         // console.log(result)
-        return res.status(200).json( {result} )
-      })
+        return res.status(200).json({ result });
+      });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
     }
-  });
+  }
+);
 
-  //Route to get the names of the children who can make a request to a particular van
-  router.post("/requestschoolchildren",security.requireAuthorizedUser, async(req, res, next)=>{
-    try{
+//Route to get the names of the children who can make a request to a particular van
+router.post(
+  "/requestschoolchildren",
+  security.requireAuthorizedUser,
+  async (req, res, next) => {
+    try {
       const username = res.locals.user.data;
-      const respond = Parent.getChildrenRequest(username,req.body)
-      respond.then(function(result){
-        console.log(result)
-        return res.status(200).json({result})
-      })
-    } catch(err){
-      next(err)
+      const respond = Parent.getChildrenRequest(username, req.body);
+      respond.then(function (result) {
+        console.log(result);
+        return res.status(200).json({ result });
+      });
+    } catch (err) {
+      next(err);
     }
-  });
+  }
+);
 
-  //Route to send a request to a particular van
-  router.post("/sendrequest", security.requireAuthorizedUser, async (req, res) => {
+//Route to send a request to a particular van
+router.post(
+  "/sendrequest",
+  security.requireAuthorizedUser,
+  async (req, res) => {
     try {
-      const request=Parent.sendRequest(req.body);
-      request.then(function(result) {
-        console.log(result)
-        return res.status(200).json( {result} )
-      })
+      const request = Parent.sendRequest(req.body);
+      request.then(function (result) {
+        console.log(result);
+        return res.status(200).json({ result });
+      });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
     }
-  });
+  }
+);
 
-  //Route to leave from a van
-  router.post("/leavevan", security.requireAuthorizedUser, async (req, res) => {
-    try {
-      const request=Parent.leaveVan(req.body);
-      request.then(function(result) {
-        console.log(result)
-        return res.status(200).json( {result} )
-      })
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server Error");
-    }
-  });
+//Route to leave from a van
+router.post("/leavevan", security.requireAuthorizedUser, async (req, res) => {
+  try {
+    const request = Parent.leaveVan(req.body);
+    request.then(function (result) {
+      console.log(result);
+      return res.status(200).json({ result });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
+//Route to mark absent
+router.post("/markabsent", security.requireAuthorizedUser, async (req, res) => {
+  try {
+    const request = Parent.markAbsent(req.body);
+    request.then(function (result) {
+      console.log(result);
+      return res.status(200).json({ result });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+router.post("/getBio", security.requireAuthorizedUser, async (req, res) => {
+  try {
+    const request = Parent.getBio(req.body);
+    request.then(function (result) {
+      console.log(result);
+      return res.status(200).json({ result });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-   //Route to mark absent
-   router.post("/markabsent", security.requireAuthorizedUser, async (req, res) => {
-    try {
-      const request=Parent.markAbsent(req.body);
-      request.then(function(result) {
-        console.log(result)
-        return res.status(200).json( {result} )
-      })
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server Error");
-    }
-  });
-  
-  module.exports = router
+module.exports = router;
